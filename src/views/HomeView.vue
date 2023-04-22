1bfc88828ce5b8f71b7e5d0b932b24ea5c21639a
Wecreds <wecredsbr@gmail.com>
2023-04-21 22:46:31 -0300

    handling api error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,19 @@ import { mapboxAPIkey } from '../_data/apikey.js'
 const searchQuery = ref('');
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref (null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async() => {
     if (searchQuery.value !== ''){
-      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIkey}&types=place`);
-      mapboxSearchResults.value = result.data.features;
-      console.log(mapboxSearchResults.value)
+      try{
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIkey}&types=place`);
+        mapboxSearchResults.value = result.data.features;
+      } catch {
+        searchError.value = true
+      }
+     
       return;
     }
     mapboxSearchResults.value = null;
@@ -28,9 +33,13 @@ const getSearchResults = () => {
       <input type="text" placeholder="Search for a city or state" class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" v-model="searchQuery"
       @input="getSearchResults">
       <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
-      <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-      {{ searchResult.place_name }}
-      </li>
+        <p v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">No result match you query, try a different term.</p>
+      <div v-else>
+        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+        {{ searchResult.place_name }}
+        </li>
+      </div>
     </ul>
     </div>
   </main>
