a4a8d445863b588020a717b034aa8925ecfd21fd
Wecreds <wecredsbr@gmail.com>
2023-04-22 21:56:56 -0300

    Handling Async Data

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,12 +1,16 @@
 <script setup>
+import AsyncitCityView from '../components/AsyncitCityView.vue';
+
 
 </script>
-
-
-
 <template>
     <div>
-
+        <Suspense>
+            <AsyncitCityView />
+            <template #fallback>
+                <p>Loading</p>
+            </template>
+        </Suspense>
     </div>
 </template>
 
