b46c4ea435fe8d5bd5c56d34ba6002b9e108e2a3
wecredsbr <wecredsbr@gmail.com>
2023-05-12 11:54:50 -0300

    City View Layout

--- src/components/AsyncitCityView.vue
+++ src/components/AsyncitCityView.vue
@@ -28,5 +28,41 @@ const weatherData = await getWeatherData();
 console.log(weatherData);
 </script>
 <template>
-  <div></div>
+  <div class="flex flex-col flex-1 items-center">
+    <!--Banner-->
+    <div v-if="route.query.preview" class="text-white text-center p-4 bg-weather-secondary w-full">
+      <p>You are currently previewing this city, click the "+" icon to start tracking this city.</p>
+    </div>
+    <!-- Weather Overview -->
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString('en-us', {
+            weekday: 'short',
+            day: '2-digit',
+            month: 'long'
+          })
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString('en-us', {
+            timeStyle: 'short'
+          })
+        }}
+      </p>
+      <p class="text-8xl mb-8">{{ Math.round(weatherData.current.temp) }}&deg;</p>
+      
+        <p>
+          Feels Like
+          {{ Math.round(weatherData.current.feels_like) }}&deg;
+        </p>
+        <p class="capitalize">
+          {{ weatherData.current.weather[0].description }}
+        </p>
+        <img
+          :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+          class="w-[150px] h-auto"
+        />
+    </div>
+  </div>
 </template>
